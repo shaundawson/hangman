05b030aba8fc698253fe37df3660e69abb68bb78
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-27 16:37:23 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Create a list of words
+word_list = ["Asexual", "Biphobia", "Deadnaming", "Homosexual", "Intersex", "Pronoun", "Queer", "Transphobia"]
+
+#Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+
+#Generate as many blanks as letters in a word
+
+
+#Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase. 
+
+
+#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+#If the guessed letter is in the word, replace the blank letter and check if all the blanks are filled. If all the blanks are filled the game is over and you win. 
+
+
+#If the guessed letter is not in the word, you lose a life. If you've run out of lives, the game is over and you lose. 
