55c6b95b8e70623552a3a7316114c00fd8883516
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-27 18:32:08 -0400

    pick random word and check letter

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+import random
+
 #Create a list of words
 word_list = ["Asexual", "Biphobia", "Deadnaming", "Homosexual", "Intersex", "Pronoun", "Queer", "Transphobia"]
 
 #Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+chosen_word = str(random.choice(word_list))
 
-
-#Generate as many blanks as letters in a word
+# Split string to each letter 
+def split(word): 
+    return [char for char in word] 
+chosen_word = (split(chosen_word.lower())) 
 
 
 #Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase. 
+guess = input("Guess a letter\n")
 
-
-#Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+#Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Generate as many blanks as letters in a word.
+word2 = []
+for l in chosen_word:  
+    if l  == guess:
+        word2.append(guess)
+    else:
+        word2.append('_')
+print(word2)
 
 #If the guessed letter is in the word, replace the blank letter and check if all the blanks are filled. If all the blanks are filled the game is over and you win. 
 
