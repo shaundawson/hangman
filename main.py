da6945805458de94a99bdbbb5bc076c057b3a829
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-28 11:26:39 -0400

    keep track of players lives

--- main.py
+++ main.py
@@ -1,5 +1,62 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 #Create a list of words
 word_list = ["Asexual", "Biphobia", "Deadnaming", "Homosexual", "Intersex", "Pronoun", "Queer", "Transphobia"]
 
@@ -23,26 +80,38 @@ for _ in range(word_length):
     display += "_"
 print(display)
 
-
+#Store number of lives left in a variable 
+lives = 6
 end_of_game = False
 
     
 # while there are blanks in display
-while not end_of_game
+while not end_of_game:
     #Ask the user to guess a letter and assign their answer to a variable called guess. 
     guess = input("Guess a letter:").lower()
     #Loop through each position in the chosen_word 
     for position in range(word_length):
         letter = chosen_word[position]
-         #If the letter in that position  matches 'guess' 
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        #If the letter in that position  matches 'guess' 
         if letter  == guess:
             #Reveal that letter in the display at postion
             display[position] = letter
+            
+    if guess not in chosen_word:
+        # if guess is not a letter in the chosen_word, reduce lives by 1
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
 
-#Show the guessed letter in the correct position and every other letter replace with "_"
-print(display)
+    #Show the guessed letter in the correct position and every other letter replace with "_"
+    print(f"{' '.join(display)}")
 
-#Check to see if it's the end of game
-if "_" not in display:
-    end_of_game = True
-    print("You win!")
+    #Check to see if it's the end of game
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+
+    #Print the ascii art from stages that corresponds with the current number of lives the user has remaining
+    print(stages[lives])
